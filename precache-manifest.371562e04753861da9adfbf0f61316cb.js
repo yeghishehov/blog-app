f8a3bba4fc762da4bc05f2bd585438309ded28a8
yeghishehov <yeghishehov.1@gmail.com>
2020-02-22 03:04:31 +0400

    Updates

---
+++ precache-manifest.371562e04753861da9adfbf0f61316cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13576382b3f696b1519162ceb7a6603",
+    "url": "/blog-app/index.html"
+  },
+  {
+    "revision": "24d963ffb559a9dc3375",
+    "url": "/blog-app/static/css/main.d0727413.chunk.css"
+  },
+  {
+    "revision": "1065ce7e01058592938b",
+    "url": "/blog-app/static/js/2.56437808.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/blog-app/static/js/2.56437808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24d963ffb559a9dc3375",
+    "url": "/blog-app/static/js/main.adfa5fdc.chunk.js"
+  },
+  {
+    "revision": "e030613defd708f04dbb",
+    "url": "/blog-app/static/js/runtime-main.5cff2fa2.js"
+  }
+]);
